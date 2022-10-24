f47865375a45299f50d58b15903896c8ebfcce3b
Leo Spratt <enchant97@users.noreply.github.com>
2022-10-24 22:56:33 +0100

    make system titles unique inside books & pages

--- api/db/models.go
+++ api/db/models.go
@@ -28,18 +28,18 @@ type Shelf struct {
 
 type Book struct {
 	BaseModel
-	ShelfID     uint   `gorm:"not null" json:"shelfId"`
+	ShelfID     uint   `gorm:"uniqueIndex:idx_books;not null" json:"shelfId"`
 	Shelf       Shelf  `gorm:"foreignKey:ShelfID" json:"-"`
 	Title       string `gorm:"not null" json:"title"`
-	SystemTitle string `gorm:"unique;not null" json:"systemTitle"`
+	SystemTitle string `gorm:"uniqueIndex:idx_books;not null" json:"systemTitle"`
 }
 
 type Page struct {
 	BaseModel
-	BookID      uint   `gorm:"not null" json:"bookId"`
+	BookID      uint   `gorm:"uniqueIndex:idx_pages;not null" json:"bookId"`
 	Book        Book   `gorm:"foreignKey:BookID" json:"-"`
 	Title       string `gorm:"not null" json:"title"`
-	SystemTitle string `gorm:"unique;not null" json:"systemTitle"`
+	SystemTitle string `gorm:"uniqueIndex:idx_pages;not null" json:"systemTitle"`
 }
 
 // FIXME Content should be binary data (bytes)
