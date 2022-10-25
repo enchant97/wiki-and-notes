f7e8a47469a21f8d894d5278df2215e50f13622f
Leo Spratt <enchant97@users.noreply.github.com>
2022-10-25 20:53:45 +0100

    split type into base

--- frontend/src/core/types.ts
+++ frontend/src/core/types.ts
@@ -2,11 +2,14 @@ export type LoginDetails = {
   apiUrl: string
 }
 
-export type User = {
+export type Base = {
   id: number
   createdAt: string
   updatedAt: string
   deletedAt?: string
+}
+
+export type User = Base & {
   username: string
 }
 
